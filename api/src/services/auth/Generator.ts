9de0e3b4f13bfe7478965506dc59d28af8f0d73a
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-12 21:39:15 +0800

    Implemented authenticator service

---
+++ api/src/services/auth/Generator.ts
@@ -0,0 +1,38 @@
+import { encode, TAlgorithm } from 'jwt-simple'
+import { APP_SECRET } from '../../config'
+import { EncodeResult, PartialSession, Session } from '../../interfaces'
+
+export default class AuthGenerator {
+	private secret: string
+	constructor() {
+		this.secret = APP_SECRET
+	}
+
+	GenerateToken(email: string, password: string): any {
+		const id = 'abc'
+		const name = password
+
+		return this.getEncodedToken({ id, email, name })
+	}
+
+	private getEncodedToken(partialSession: PartialSession): EncodeResult {
+		const algorithm: TAlgorithm = 'HS512'
+
+		// Determine when the token should expire
+		const issued = Date.now()
+		const fifteenMinutesInMs = 15 * 60 * 1000
+		const expires = issued + fifteenMinutesInMs
+
+		const session: Session = {
+			...partialSession,
+			issued: issued,
+			expires,
+		}
+
+		return {
+			token: encode(session, this.secret, algorithm),
+			issued: issued,
+			expires: expires,
+		}
+	}
+}
