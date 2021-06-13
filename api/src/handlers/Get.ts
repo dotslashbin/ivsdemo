656e368d8393dcb8ea482b7480013f86de3e6869
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-13 10:47:06 +0800

    Initial implementation of get all

--- api/src/handlers/Get.ts
+++ api/src/handlers/Get.ts
@@ -2,10 +2,15 @@ import { Request, Response } from 'express'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const GetAll = async (request: Request, response: Response) => {
-	console.log(request.params)
+	// eslint-disable-next-line no-console
+	const page = request.query.page
+	const limit = request.query.limit
+
+	// eslint-disable-next-line no-console
+	console.log(page, limit, request.query)
 
 	response.status(200)
-	response.json({ sulod: 'all the data here ' })
+	response.json(response.locals)
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
